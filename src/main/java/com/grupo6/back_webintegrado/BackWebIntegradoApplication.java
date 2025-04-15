58ccb85abf61be9518c3bbaec94ba2a9f577be6c
Jhoel Alonso <jtinipuclla2@gmail.com>
2025-04-14 19:52:36 -0500

    Prueba

---
+++ src/main/java/com/grupo6/back_webintegrado/BackWebIntegradoApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6.back_webintegrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackWebIntegradoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackWebIntegradoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grupo6/back_webintegrado/BackWebIntegradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6.back_webintegrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackWebIntegradoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
